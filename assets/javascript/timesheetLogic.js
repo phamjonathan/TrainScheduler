fcbac031a5da9957dba2308852e5ed4ce4aa81f0
Jonathan Pham <jonathanthienpham@gmail.com>
2020-02-29 09:34:50 -0500

    form fix

--- assets/javascript/timesheetLogic.js
+++ assets/javascript/timesheetLogic.js
@@ -1,13 +1,4 @@
-// Steps to complete:
 
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
 
 var firebaseConfig = {
   apiKey: "AIzaSyDBeZdSbcdj4qrZwtg2QUTfCZIenXTMzm4",
@@ -59,7 +50,7 @@ $("#add-train-btn").on("click", function(event) {
   $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding train name to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
 
@@ -154,10 +145,4 @@ database.ref().on("child_added", function(childSnapshot) {
   $("#train-table > tbody").append(newRow);
 });
 
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
 
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
